904cab044e797debdd1c3b9ad910e8472ecb9c63
Hugo N. Lima <hugo.hnl@gmail.com>
2022-02-04 16:42:33 -0300

    Atualização dos arquivos do projeto

--- src/global/global.js
+++ src/global/global.js
@@ -1,11 +1,6 @@
 import header from './Header/Header'
 import footer from './Footer/Footer'
 
-// import './utils/vtexSmartResearch'
-// import './utils/breadcrumb'
-// import './utils/minicart'
-// import './utils/shelf'
-
 import './global.scss'
 
 const main = () => {
